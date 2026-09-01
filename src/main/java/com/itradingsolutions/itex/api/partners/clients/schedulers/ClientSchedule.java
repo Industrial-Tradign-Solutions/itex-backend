0e9f56c2d71b634a6c37070cf27d2f6c71de605d
Julian David Bayer <jdbayer09@hotmail.com>
2026-08-31 19:51:08 -0500

    Ajuste de notificaciones completo

--- src/main/java/com/itradingsolutions/itex/api/partners/clients/schedulers/ClientSchedule.java
+++ src/main/java/com/itradingsolutions/itex/api/partners/clients/schedulers/ClientSchedule.java
@@ -71,11 +71,10 @@ public class ClientSchedule {
      * Funcion para notificar errores en los clientes
      * 1. Notificacion de AccountRep IP
      * */
-    //@Scheduled(cron = "0 0 5 * * 3")
-    @Scheduled(cron = "0 42 19 * * *")
+    @Scheduled(cron = "0 0 5 * * 3")
     private void sendActiveClientNotification() {
         var listClients = clientService.listAllByStatus(ClientStatus.ACTIVE);
-        //sendNotificationClientNotAssignedToAccountRepByDep(listClients, Departments.IP);
+        sendNotificationClientNotAssignedToAccountRepByDep(listClients, Departments.IP);
         //sendNotificationClientNotAssignedToAccountRepByDep(listClients, Departments.ACC);
         sendNotificationClientWhitMissingInfo(listClients);
     }
@@ -151,106 +150,6 @@ public class ClientSchedule {
         }
         return listMissingInfo;
     }
-    /*
-    @Scheduled(cron = "0 10 5 * * 3")
-    private void sendNotificationsInfo() {
-        var listClients = clientService.listAllWhitMissingInfo();
-        var mails = getListMails(listClients);
-        var infoToMails = getMapClients(mails);
-        sendNotifications(infoToMails, "Notification of Clients with missing information", "The following clients do not have complete information: ");
-    }
-
-    private static final String CLIENT_NAME = "client";
-    private static final String USER_MAIL_NAME = "userMail";
-    private static final String USER_FULL_NAME = "userFullName";
-
-    private List<ClientMissingInfoEmail> getListMailsV2(List<ClientMissingInfo> listClients) {
-        List<ClientMissingInfoEmail> mails = new ArrayList<>();
-        listClients.forEach(clientMissingInfo -> {
-            var listDepInfo = clientMissingInfo.client().getInfoByDepartment().stream().filter(info -> info.getAccountRep() != null).toList();
-            if (listDepInfo.isEmpty()) {
-                mails.add(new ClientMissingInfoEmail(
-                    clientMissingInfo.client().getName(),
-                    clientMissingInfo.client().getCode(),
-                    clientMissingInfo.client().getUpdatedBy() != null ? clientMissingInfo.client().getUpdatedBy().getFullName(): clientMissingInfo.client().getCreatedBy().getFullName(),
-                    clientMissingInfo.client().getUpdatedBy() != null ? clientMissingInfo.client().getUpdatedBy().getEmail(): clientMissingInfo.client().getCreatedBy().getEmail(),
-                    clientMissingInfo.errors()
-                ));
-            } else {
-                listDepInfo.forEach(depInfo -> mails.add(new ClientMissingInfoEmail(
-                        clientMissingInfo.client().getName(),
-                        clientMissingInfo.client().getCode(),
-                        depInfo.getAccountRep().getFullName(),
-                        depInfo.getAccountRep().getEmail(),
-                        clientMissingInfo.errors()
-                )));
-            }
-        });
-        return mails;
-    }
-
-    private List<Map<String, String>> getListMails(List<ClientMissingInfo> listClients) {
-        List<Map<String, String>> mails = new ArrayList<>();
-
-        listClients.forEach(clientMissingInfo -> {
-            var listDepInfo = clientMissingInfo.client().getInfoByDepartment().stream().filter(info -> info.getAccountRep() != null).toList();
-
-            if (listDepInfo.isEmpty()) {
-                Map<String, String> item = new HashMap<>();
-                item.put(CLIENT_NAME, clientMissingInfo.client().getCode() + "-" + clientMissingInfo.client().getName());
-
-                if (clientMissingInfo.client().getUpdatedBy() != null) {
-                    item.put(USER_MAIL_NAME, clientMissingInfo.client().getUpdatedBy().getEmail());
-                    item.put(USER_FULL_NAME, clientMissingInfo.client().getUpdatedBy().getFullName());
-                } else {
-                    item.put(USER_MAIL_NAME, clientMissingInfo.client().getCreatedBy().getEmail());
-                    item.put(USER_FULL_NAME, clientMissingInfo.client().getCreatedBy().getFullName());
-                }
-
-                mails.add(item);
-            } else {
-                listDepInfo.forEach(depInfo -> {
-                    Map<String, String> item = new HashMap<>();
-                    item.put(CLIENT_NAME, clientMissingInfo.client().getCode() + "-" + clientMissingInfo.client().getName());
-                    item.put(USER_MAIL_NAME, depInfo.getAccountRep().getEmail());
-                    item.put(USER_FULL_NAME, depInfo.getAccountRep().getFullName());
-                    mails.add(item);
-                });
-            }
-        });
-        return mails;
-    }
-
-    private Map<String, Map<String, Object>> getMapClients(List<Map<String, String>> mails) {
-        return mails.stream()
-                .filter(map -> map.get(USER_MAIL_NAME) != null)
-                .collect(Collectors.groupingBy(
-                        map -> map.get(USER_MAIL_NAME),
-                        Collectors.collectingAndThen(
-                                Collectors.toList(),
-                                list -> {
-                                    Map<String, Object> newMap = new HashMap<>();
-                                    newMap.put(CLIENT_NAME, list.stream()
-                                            .map(map -> map.get(CLIENT_NAME))
-                                            .toList());
-                                    newMap.put(USER_FULL_NAME, list.stream()
-                                            .map(map -> map.get(USER_FULL_NAME))
-                                            .findFirst()
-                                            .orElse(""));
-                                    return newMap;
-                                }
-                        )
-
-                ));
-    }
-
-    private void sendNotifications(Map<String, Map<String, Object>> infoToMails, String subject, String message) {
-        infoToMails.forEach((userMail, dataList) ->
-            sendMail((List<String>) dataList.get(CLIENT_NAME), subject, message, userMail, (String) dataList.get(USER_FULL_NAME))
-        );
-    }
-
-*/
 
     private void sendMail(List<ClientMissingInfo> clientMissingInfos, String subject, String message, String userMail, String userFullName) {
         var clientsTemplate = getClientsTemplate(clientMissingInfos);

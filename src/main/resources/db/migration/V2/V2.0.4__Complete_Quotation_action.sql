84179bfc41b28f844700ef37798a9bf79ba9be78
Julian David Bayer <jdbayer09@hotmail.com>
2026-09-08 16:14:31 -0500

    db(migration): add V2.0.4 Complete Quotation action
    - Insert COMPLETE_IP_QUOTATIONS (4003007) into t_actions
    - Add rollback script for V2.0.4

---
+++ src/main/resources/db/migration/V2/V2.0.4__Complete_Quotation_action.sql
@@ -0,0 +1,2 @@
+INSERT INTO t_actions (id, name, description, menu_item_id, is_active, created_at)
+VALUES (4003007, 'Complete Quotation', 'Allows you to manually set an ANSWERED Quotation to COMPLETE status', 4003, true, now());
